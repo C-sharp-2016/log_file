ec658d6b7a16c06d878dd3bcc6174234e8ca429d
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-08-03 00:40:08 +0800

    initialized data and logging text works

---
+++ log_file/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace log_file
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.press_log_button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // press_log_button1
+            // 
+            this.press_log_button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.press_log_button1.Location = new System.Drawing.Point(37, 84);
+            this.press_log_button1.Name = "press_log_button1";
+            this.press_log_button1.Size = new System.Drawing.Size(202, 99);
+            this.press_log_button1.TabIndex = 0;
+            this.press_log_button1.Text = "Press To Log Text To File";
+            this.press_log_button1.UseVisualStyleBackColor = true;
+            this.press_log_button1.Click += new System.EventHandler(this.press_log_button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.press_log_button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button press_log_button1;
+    }
+}
+
---
+++ log_file/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+ 
+namespace log_file
+{
+    public partial class Form1 : Form
+    { 
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+         
+        private void press_log_button1_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter w = File.AppendText("log/log.txt"))
+            {
+                Log("This is the log of my application here you can wether it is running correctly, please be reminded the this is great application!", w); 
+            }
+
+            using (StreamReader r = File.OpenText("log/log.txt"))
+            {
+                DumpLog(r);
+            }
+        } 
+         
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.Write("\r\nLog Entry : ");
+            w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString()); 
+            w.WriteLine("  :{0}", logMessage);
+            w.WriteLine("----------------------------------------------------------------------------------");
+        } 
+
+        public static void DumpLog(StreamReader r)
+        {
+            string line;
+            while ((line = r.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
